62a61bd866ab1cb677a35527ee4c9c7e0f803150
Sisypheus <contact@sisypheus.dev>
2024-02-29 15:47:03 +0100

    feat(parser): added main Parser module entry

---
+++ src/Language/Feather/Parser.hs
@@ -0,0 +1,20 @@
+module Language.Feather.Parser where
+
+import Control.Monad.State
+import Data.Void
+import Language.Feather.CST
+import Language.Feather.Parser.Lexer
+import Language.Feather.Parser.Modules.Expression
+import Text.Megaparsec
+
+type FileContent = String
+
+type ParsingError = ParseErrorBundle FileContent Void
+
+parseTest :: FileContent -> Either ParsingError Program
+parseTest s = parseFeatherFile "" s
+
+parseFeatherFile :: FilePath -> FileContent -> Either ParsingError Program
+parseFeatherFile filePath fileContent =
+  fst $
+    runState (runParserT (scn *> parseProgram) filePath fileContent) 0
