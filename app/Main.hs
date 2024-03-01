d7d43e17f6537060e43da6bc5cee8d9699d0835e
Sisypheus <contact@sisypheus.dev>
2024-03-01 09:35:30 +0100

    refactor(main): updated entry app to match relude

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,17 @@
 module Main where
 
+import Data.Text.IO
 import Language.Feather.Parser
 import Text.Megaparsec (errorBundlePretty)
+import Prelude hiding (print, putStrLn, readFile)
+
+printText :: (ToText a) => a -> IO ()
+printText = putStrLn . toText
 
 main :: IO ()
 main = do
   content <- readFile "example/main.fth"
   let x = parseTest content
   case x of
-    Left e -> putStrLn $ errorBundlePretty e
-    Right r -> mapM_ print r
+    Left e -> putStrLn . toText $ errorBundlePretty e
+    Right r -> mapM_ printText r
