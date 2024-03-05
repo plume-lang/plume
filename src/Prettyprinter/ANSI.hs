ad31084ff61533a572829bf88186a3f99fabdbf6
Sisypheus <contact@sisypheus.dev>
2024-03-05 16:12:40 +0100

    style(pprint): added ANSI Pretty typeclass

---
+++ src/Prettyprinter/ANSI.hs
@@ -0,0 +1,23 @@
+module Prettyprinter.ANSI where
+
+import Prettyprinter
+import Prettyprinter.Render.Terminal
+
+class ANSIPretty a where
+  ansiPretty :: a -> Doc AnsiStyle
+
+instance (ANSIPretty a) => ANSIPretty [a] where
+  ansiPretty = hsep . map ansiPretty
+
+instance (ANSIPretty a) => ANSIPretty (Maybe a) where
+  ansiPretty Nothing = mempty
+  ansiPretty (Just a) = ansiPretty a
+
+anCol :: Color -> Doc AnsiStyle -> Doc AnsiStyle
+anCol = annotate . color
+
+anBold :: Doc AnsiStyle -> Doc AnsiStyle
+anBold = annotate bold
+
+anItalic :: Doc AnsiStyle -> Doc AnsiStyle
+anItalic = annotate italicized
