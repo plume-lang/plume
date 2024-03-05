0e37f928911398749efabc67d663913f3f37de5d
Sisypheus <contact@sisypheus.dev>
2024-03-05 16:13:24 +0100

    feat(log): added bunch of IO functions

---
+++ src/Print.hs
@@ -0,0 +1,16 @@
+module Print where
+
+import Data.Text.IO
+import Prettyprinter.ANSI
+import Prettyprinter.CST ()
+import Prettyprinter.Render.Terminal
+import Prelude hiding (print, putStrLn, readFile)
+
+printText :: (ToText a) => a -> IO ()
+printText = putStrLn . toText
+
+ppPut :: (ANSIPretty a) => a -> IO ()
+ppPut = putDoc . ansiPretty
+
+ppPrint :: (ANSIPretty a) => a -> IO ()
+ppPrint x = ppPut x >> putStrLn ""
