197373f6dd88ef723db44474983539e92d9cc1c2
Sisypheus <contact@sisypheus.dev>
2024-03-05 16:14:24 +0100

    test(entry): added Print module imports

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,14 @@ module Main where
 
 import Data.Text.IO
 import Language.Feather.Parser
+import Print
 import Text.Megaparsec (errorBundlePretty)
-import Prelude hiding (print, putStrLn, readFile)
-
-printText :: (ToText a) => a -> IO ()
-printText = putStrLn . toText
+import Prelude hiding (readFile)
 
 main :: IO ()
 main = do
   content <- readFile "example/main.fth"
   let x = parseTest content
   case x of
-    Left e -> putStrLn . toText $ errorBundlePretty e
-    Right r -> mapM_ printText r
+    Left e -> printText $ errorBundlePretty e
+    Right r -> ppPrint r
