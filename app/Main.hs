21ea1ee78f1e2fde7007c1e2e78ecacd32c8c29d
Sisypheus <contact@sisypheus.dev>
2024-03-06 23:18:45 +0100

    refactor(ext): updated file extension in code and repository

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,15 @@
 module Main where
 
 import Data.Text.IO
-import Language.Plume.Parser
+import Plume.Syntax.Concrete.Internal.Pretty ()
+import Plume.Syntax.Parser
 import Print
 import Text.Megaparsec (errorBundlePretty)
 import Prelude hiding (readFile)
 
 main :: IO ()
 main = do
-  content <- readFile "example/main.fth"
+  content <- readFile "example/main.plm"
   let x = parseTest content
   case x of
     Left e -> printText $ errorBundlePretty e
