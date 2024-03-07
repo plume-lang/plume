595892ce45d1a76dcccab0b6a6e68d2b4611ea3e
Sisypheus <contact@sisypheus.dev>
2024-03-07 16:46:20 +0100

    feat(monad/IO): added IOReader type utility

---
+++ src/Control/Monad/IO.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Control.Monad.IO where
+
+import Prelude hiding (modify)
+
+-- We're using the `mtl` library to define our own monad transformer stack.
+-- That's kind of the only monad we're using in this project because it's
+-- simple, easy to manipulate and understand and also complete for our needs.
+--
+-- The goal of this monad is to represent many monadic operations in one:
+-- - IO operations obviously
+-- - Reader operations to pass around the environment
+-- - State by using IORef for instance
+-- - Error handling with throwIO function
+type IOReader env a = ReaderT env IO a
+
+delete :: (Monoid a, MonadIO m) => IORef a -> m ()
+delete ref = liftIO $ writeIORef ref mempty
