5cba5b811f719021b72f692085e4eeb7637880dc
Sisypheus <contact@sisypheus.dev>
2024-03-09 16:40:45 +0100

    test(entry): replaced old pattern with new catchIO function

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,18 @@
 module Main where
 
+import Control.Monad.Exception
 import Data.Text.IO
 import Plume.Syntax.Abstract.Internal.Pretty ()
 import Plume.Syntax.Parser
 import Plume.Syntax.Translation.ConcreteToAbstract
 import System.IO.Pretty
-import Text.Megaparsec (errorBundlePretty)
 import Prelude hiding (readFile)
 
 main :: IO ()
 main = do
   let file = "example/main.plm"
   content <- readFile file
-  x <- parsePlumeFile file content
-  case x of
-    Left e -> printText $ errorBundlePretty e
-    Right r -> do
-      ast <- sequence <$> mapM concreteToAbstract r
-      case ast of
-        Left e -> printText e
-        Right r' -> ppPrint r'
+
+  catchIO (parsePlumeFile file content) $ \cst -> do
+    catchIO (sequence <$> mapM concreteToAbstract cst) $ \ast -> do
+      ppPrint ast
