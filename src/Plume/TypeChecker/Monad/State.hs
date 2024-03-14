7daa3399fc7de19c5674a67fea7024968d761693
Sisypheus <contact@sisypheus.dev>
2024-03-14 23:30:22 +0100

    feat(tc): added HasField instances for Checker state

--- src/Plume/TypeChecker/Monad/State.hs
+++ src/Plume/TypeChecker/Monad/State.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
 
 module Plume.TypeChecker.Monad.State where
 
+import Control.Monad.Except
 import Data.Map qualified as Map
 import GHC.IO
 import GHC.Records
 import Plume.Syntax.Concrete (Position)
 import Plume.TypeChecker.Constraints.Definition
 import Plume.TypeChecker.Monad.Type
+import Plume.TypeChecker.Monad.Type.Error
 import Plume.TypeChecker.Monad.Type.Scheme
 
 type Environment = Map Text Scheme
@@ -40,9 +43,33 @@ emptyState =
 
 search
   :: forall l v m
-   . (HasField l CheckerState (Map Text v), MonadIO m)
+   . ( HasField l CheckerState (Map Text v)
+     , MonadIO m
+     , MonadError (TypeError, Maybe Position) m
+     )
   => Text
   -> m (Maybe v)
 search k = do
   v <- readIORef checkerST
   return $ Map.lookup k (getField @l v)
+
+instance HasField "variables" CheckerState (Map Text Scheme) where
+  hasField c = (\x -> c {variables = x}, variables c)
+
+instance HasField "types" CheckerState (Map Text Scheme) where
+  hasField c = (\x -> c {types = x}, types c)
+
+instance HasField "returnType" CheckerState PlumeType where
+  hasField c = (\x -> c {returnType = x}, returnType c)
+
+instance HasField "position" CheckerState (Maybe Position) where
+  hasField c = (\x -> c {position = x}, position c)
+
+instance HasField "generics" CheckerState (Map Text Int) where
+  hasField c = (\x -> c {generics = x}, generics c)
+
+instance HasField "constraints" CheckerState [TypeConstraint] where
+  hasField c = (\x -> c {constraints = x}, constraints c)
+
+instance HasField "tvarCounter" CheckerState Int where
+  hasField c = (\x -> c {tvarCounter = x}, tvarCounter c)
