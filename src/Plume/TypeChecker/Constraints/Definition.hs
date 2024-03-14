9b78fc6df78d29aacfb30dd913e6d6e9cbd014a5
Sisypheus <contact@sisypheus.dev>
2024-03-14 16:19:08 +0100

    feat(constraints): added extension constraint

--- src/Plume/TypeChecker/Constraints/Definition.hs
+++ src/Plume/TypeChecker/Constraints/Definition.hs
@@ -6,13 +6,16 @@ import Plume.TypeChecker.Monad
 
 data ConstraintConstructor
   = PlumeType :~: PlumeType
+  | PlumeType `Extends` Text
   | Hole PlumeType
 
 type TypeConstraint = (Position, ConstraintConstructor)
 
 instance Types ConstraintConstructor where
   free (t1 :~: t2) = free t1 `S.union` free t2
+  free (t `Extends` _) = free t
   free (Hole t) = free t
 
   apply s (t1 :~: t2) = apply s t1 :~: apply s t2
   apply s (Hole t) = Hole $ apply s t
+  apply s (t `Extends` c) = apply s t `Extends` c
--- src/Plume/TypeChecker/Constraints/Solver.hs
+++ src/Plume/TypeChecker/Constraints/Solver.hs
@@ -1,17 +1,23 @@
 module Plume.TypeChecker.Constraints.Solver where
 
+import Control.Monad.Except
 import Plume.Syntax.Concrete (Position)
 import Plume.TypeChecker.Constraints.Definition
 import Plume.TypeChecker.Constraints.Unification
 import Plume.TypeChecker.Monad
 
-solve :: [TypeConstraint] -> Either (TypeError, Position) Substitution
+solve
+  :: (MonadReader Environment m, MonadError (TypeError, Position) m)
+  => [TypeConstraint]
+  -> m Substitution
 solve [] = return mempty
 solve ((pos, t1 :~: t2) : xs) = do
   let s1 = mgu t1 t2
   case s1 of
-    Left err -> Left (err, pos)
+    Left err -> throwError (err, pos)
     Right s1' -> do
       let s2 = solve $ map (second (apply s1')) xs
       compose s1' <$> s2
+solve ((pos, _ `Extends` _) : _) =
+  throwError (CompilerError "Not implemented", pos)
 solve ((_, Hole _) : xs) = solve xs
