68191b1b022c7c444217ac16e1e20f1482954313
Sisypheus <contact@sisypheus.dev>
2024-03-15 10:31:22 +0100

    test(entry): now printing TLIR output

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,19 @@ module Main where
 
 import Control.Monad.Exception
 import Data.Text.IO
-import Plume.Syntax.Abstract.Internal.Pretty ()
 import Plume.Syntax.Parser
 import Plume.Syntax.Translation.ConcreteToAbstract
 import Plume.TypeChecker.Checker hiding (with)
+import Plume.TypeChecker.TLIR.Internal.Pretty ()
+import System.IO.Pretty
 import Prelude hiding (readFile)
 
 main :: IO ()
 main = do
-  let file = "example/main.plm"
+  let file = "example/typecheck.plm"
   content <- readFile file
 
   parsePlumeFile file content `with` \cst -> do
     runConcreteToAbstract cst `with` \ast -> do
-      runSynthesize ast `with` \_ -> do
-        print @Text "done"
+      runSynthesize ast `with` \tlir -> do
+        ppPrint tlir
