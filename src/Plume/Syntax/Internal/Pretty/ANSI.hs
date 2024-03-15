24c59bd46ac186ca2b324fb687a4ffe328395671
Sisypheus <contact@sisypheus.dev>
2024-03-15 10:28:44 +0100

    feat(prettyprint): added prettyprinting for tuples

--- src/Plume/Syntax/Internal/Pretty/ANSI.hs
+++ src/Plume/Syntax/Internal/Pretty/ANSI.hs
@@ -17,6 +17,9 @@ instance (ANSIPretty a) => ANSIPretty (Maybe a) where
   ansiPretty (Just a) = ansiPretty a
   ansiPretty Nothing = mempty
 
+instance (ANSIPretty a, ANSIPretty b) => ANSIPretty (a, b) where
+  ansiPretty (a, b) = hsep $ punctuate comma [ansiPretty a, ansiPretty b]
+
 class ANSIPretty a where
   ansiPretty :: a -> Doc AnsiStyle
   default ansiPretty :: (Pretty a) => a -> Doc AnsiStyle
