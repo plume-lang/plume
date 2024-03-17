e11a6dae0f2322e1a2379e28d01d4b8b3dea5849
Sisypheus <contact@sisypheus.dev>
2024-03-17 22:56:51 +0100

    test(entry): added closure conversion algorithm call in entry

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,20 @@ module Main where
 
 import Control.Monad.Exception
 import Data.Text.IO
+import Plume.Compiler.ClosureConversion.Conversion
 import Plume.Syntax.Parser
 import Plume.Syntax.Translation.ConcreteToAbstract
 import Plume.TypeChecker.Checker
 import Plume.TypeChecker.TLIR.Internal.Pretty ()
-import System.IO.Pretty
 import Prelude hiding (readFile)
 
 main :: IO ()
 main = do
-  let file = "example/typecheck.plm"
+  let file = "example/closure.plm"
   content <- readFile file
 
   parsePlumeFile file content `with` \cst -> do
     runConcreteToAbstract cst `with` \ast -> do
       runSynthesize ast `with` \tlir -> do
-        ppPrint tlir
+        runClosureConversion tlir `with` \closed -> do
+          mapM_ print closed
