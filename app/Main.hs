7d488b4e49fdf6fa7d3db0b5e5d4cfba53e61b0d
Sisypheus <contact@sisypheus.dev>
2024-03-18 16:58:58 +0100

    test(entry): now running type erasure and desugaring steps

--- app/Main.hs
+++ app/Main.hs
@@ -3,19 +3,26 @@ module Main where
 import Control.Monad.Exception
 import Data.Text.IO
 import Plume.Compiler.ClosureConversion.Conversion
+import Plume.Compiler.Desugaring.Desugar
+import Plume.Compiler.TypeErasure.EraseType
 import Plume.Syntax.Parser
 import Plume.Syntax.Translation.ConcreteToAbstract
 import Plume.TypeChecker.Checker
 import Plume.TypeChecker.TLIR.Internal.Pretty ()
+import System.IO.Pretty
 import Prelude hiding (readFile)
 
 main :: IO ()
 main = do
-  let file = "example/closure.plm"
+  let file = "example/typecheck.plm"
   content <- readFile file
 
   parsePlumeFile file content `with` \cst -> do
     runConcreteToAbstract cst `with` \ast -> do
       runSynthesize ast `with` \tlir -> do
+        ppPrint tlir
         runClosureConversion tlir `with` \closed -> do
-          mapM_ print closed
+          let erased = eraseType closed
+          desugared <- desugar erased
+
+          mapM_ print desugared
