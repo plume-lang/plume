41c52f71d4e0d6df1a38b354eb5567bce3185f99
Sisypheus <contact@sisypheus.dev>
2024-03-18 16:56:25 +0100

    feat(type-erasure): added type erased syntax

---
+++ src/Plume/Compiler/TypeErasure/Syntax.hs
@@ -0,0 +1,36 @@
+module Plume.Compiler.TypeErasure.Syntax where
+
+import Plume.Syntax.Common.Literal
+
+data UntypedExpr
+  = UEVar Text
+  | UEApplication UntypedExpr [UntypedExpr]
+  | UELiteral Literal
+  | UEList [UntypedExpr]
+  | UEDeclaration Text UntypedExpr UntypedExpr
+  | UEConditionBranch UntypedExpr UntypedExpr UntypedExpr
+  | UESwitch UntypedExpr [(UntypedPattern, UntypedExpr)]
+  | UEBlock [UntypedStatement]
+  | UEProperty UntypedExpr Int
+  | UEDictionary (IntMap UntypedExpr)
+  | UETypeOf UntypedExpr
+  deriving (Eq, Show, Ord)
+
+data UntypedPattern
+  = UPVariable Text
+  | UPLiteral Literal
+  | UPConstructor Text [UntypedPattern]
+  | UPWildcard
+  deriving (Eq, Show, Ord)
+
+data UntypedStatement
+  = USExpr UntypedExpr
+  | USReturn UntypedExpr
+  | USDeclaration Text UntypedExpr
+  deriving (Eq, Show, Ord)
+
+data UntypedProgram
+  = UPFunction Text [Text] UntypedStatement
+  | UPStatement UntypedStatement
+  | UPNativeFunction Text Int
+  deriving (Eq, Show, Ord)
