ba8537547fdae5a18f3baf14e8c763377f4ea267
Sisypheus <contact@sisypheus.dev>
2024-03-20 18:53:20 +0100

    test(entry): added compiler passes calls

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,31 @@
 module Main where
 
 import Control.Monad.Exception
-import Data.Text.IO
+import Data.Text.IO hiding (writeFile)
+import Plume.Compiler.Bytecode.Assembler
+import Plume.Compiler.Bytecode.Serialize
 import Plume.Compiler.ClosureConversion.Conversion
 import Plume.Compiler.Desugaring.Desugar
+import Plume.Compiler.SSA
 import Plume.Compiler.TypeErasure.EraseType
 import Plume.Syntax.Parser
 import Plume.Syntax.Translation.ConcreteToAbstract
 import Plume.TypeChecker.Checker
 import Plume.TypeChecker.TLIR.Internal.Pretty ()
-import System.IO.Pretty
 import Prelude hiding (readFile)
 
 main :: IO ()
 main = do
-  let file = "example/typecheck.plm"
+  let file = "example/closure.plm"
   content <- readFile file
 
   parsePlumeFile file content `with` \cst -> do
     runConcreteToAbstract cst `with` \ast -> do
       runSynthesize ast `with` \tlir -> do
-        ppPrint tlir
         runClosureConversion tlir `with` \closed -> do
           let erased = eraseType closed
           desugared <- desugar erased
-
-          mapM_ print desugared
+          let ssa = runSSA desugared
+          bytecode <- assembleBytecode ssa
+          sbc <- serialize bytecode
+          writeFileLBS "example/closure.plm.bc" sbc
