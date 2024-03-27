192700eace1e0ff6f8d1dc0f5317fa9ab9ae10bf
Sisypheus <contact@sisypheus.dev>
2024-03-27 14:43:46 +0100

    docs(example): added basic FFI and module example

--- example/ffi.c
+++ example/ffi.c
@@ -1,3 +1,5 @@
+#include <core/error.h>
+#include <module.h>
 #include <stdio.h>
 #include <value.h>
 
@@ -7,6 +9,16 @@ int facto(int n) {
   return n * facto(n - 1);
 }
 
-Value test(int argc, Value *argv) {
+Value test(int argc, Module *m, Value *argv) {
   return MAKE_INTEGER(facto(argv[0].int_value));
+}
+
+Value mul(int argc, Module *m, Value *argv) {
+  ASSERT(argc == 2, "mul expects 2 arguments");
+  return MAKE_INTEGER(argv[0].int_value * argv[1].int_value);
+}
+
+Value sub(int argc, Module *m, Value *argv) {
+  ASSERT(argc == 2, "sub expects 2 arguments");
+  return MAKE_INTEGER(argv[0].int_value - argv[1].int_value);
 }
