6008600615a5e28b4a228f78e20234f7ef7c6798
Sisypheus <contact@sisypheus.dev>
2024-03-27 16:53:00 +0100

    feat(std): added get_args function

--- standard/natives/io.c
+++ standard/natives/io.c
@@ -4,10 +4,7 @@
 #include <string.h>
 #include <value.h>
 
-Value print(int arg_n, Module* mod, Value* args) {
-  if (arg_n < 1) THROW("Print expects at least 1 argument");
-  Value v = args[0];
-
+void print_helper(Value v) {
   switch (v.type) {
     case VALUE_INT:
       printf("%lld", v.int_value);
@@ -16,11 +13,33 @@ Value print(int arg_n, Module* mod, Value* args) {
       printf("%f", v.float_value);
       break;
     case VALUE_STRING:
-      printf("%s", v.string_value);
+      printf("\"%s\"", v.string_value);
       break;
-    default:
-      THROW("Print expects integer, float, or string arguments");
+    case VALUE_LIST:
+      printf("[");
+      for (int i = 0; i < v.list_value.length; i++) {
+        print_helper(v.list_value.values[i]);
+        if (i < v.list_value.length - 1) printf(", ");
+      }
+      printf("]");
+      break;
+    case VALUE_ADDRESS:
+      printf("<function 0x%x>", v.address_value);
+      break;
+    case VALUE_NATIVE:
+      printf("<native>");
+      break;
+    case VALUE_SPECIAL:
+      printf("<special>");
+      break;
   }
+}
+
+Value print(int arg_n, Module* mod, Value* args) {
+  if (arg_n < 1) THROW("Print expects at least 1 argument");
+  Value v = args[0];
+
+  print_helper(v);
 
   return MAKE_INTEGER(0);
 }
@@ -29,4 +48,8 @@ Value println(int arg_n, Module* mod, Value* args) {
   print(arg_n, mod, args);
   printf("\n");
   return MAKE_INTEGER(0);
+}
+
+Value get_args(int arg_n, Module* mod, Value* args) {
+  return MAKE_LIST(mod->args);
 }
