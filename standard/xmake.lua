83e8f0200712500cc36046596574fff8036af8c9
Sisypheus <contact@sisypheus.dev>
2024-03-31 22:51:14 +0200

    build(std): updated standard library xmake file

--- standard/xmake.lua
+++ standard/xmake.lua
@@ -1,9 +1,9 @@
 add_rules("mode.debug", "mode.release", "mode.profile")
 
 target("plume-natives")
-  add_files("natives/**.c")
+  add_files("ffi/**.c")
   add_includedirs("../runtime/include")
   set_kind("shared")
   set_targetdir(".")
-  set_basename("natives")
+  set_basename("native")
   set_prefixname("")
