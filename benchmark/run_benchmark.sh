7a6544f61ad6132c9d9c6eab2fda54fd104d3a1f
Sisypheus <contact@sisypheus.dev>
2024-04-01 22:33:18 +0200

    docs(benchmark): added new benchmark results

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -2,9 +2,8 @@ cd benchmark
 rustc index.rs -C opt-level=3 -o index-rs
 javac Main.java
 # gcc -O3 benchmark/index.c -o index-c
-bin/plume-language
 hyperfine \
-  "python3 index.py" \
+  "pypy3.10 index.py" \
   "node index.js" \
   "./index-rs" \
   "java Main" \
