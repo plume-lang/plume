ece23e88890dcfdee8dc396f1580d64dad665d9e
Sisypheus <contact@sisypheus.dev>
2024-04-05 16:26:26 +0200

    feat(std): added slicing methods for str and list

--- standard/ffi/string.c
+++ standard/ffi/string.c
@@ -209,3 +209,30 @@ Value eq_char(int arg_n, Module* mod, Value* args) {
 
   return MAKE_INTEGER(x == y);
 }
+
+Value str_slice(size_t argc, Module* mod, Value* args) {
+  ASSERT_FMT(argc == 3, "Expected 3 arguments, but got %zu", argc);
+
+  char* str = args[0].string_value;
+  int start = args[1].int_value;
+  int end = args[2].int_value;
+
+  int len = strlen(str);
+
+  if (start < 0) start = len + start;
+  if (end < 0) end = len + end;
+
+  if (start < 0) start = 0;
+  if (end < 0) end = 0;
+
+  if (start > len) start = len;
+  if (end > len) end = len;
+
+  if (start > end) start = end;
+
+  char* slice = malloc((end - start + 1) * sizeof(char));
+  strncpy(slice, str + start, end - start);
+  slice[end - start] = '\0';
+
+  return MAKE_STRING(slice);
+}
