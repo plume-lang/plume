71becceee19ddbc291c65ef1bcd872cad0def103
Sisypheus <contact@sisypheus.dev>
2024-04-09 11:58:39 +0200

    feat(tc): added datatype information ref

--- src/Plume/TypeChecker/Checker/Datatype.hs
+++ src/Plume/TypeChecker/Checker/Datatype.hs
@@ -1,5 +1,7 @@
 module Plume.TypeChecker.Checker.Datatype where
 
+import Data.Map qualified as M
+import GHC.IO
 import Plume.Syntax.Abstract qualified as Pre
 import Plume.Syntax.Common.Annotation
 import Plume.Syntax.Common.Type qualified as Pre
@@ -7,6 +9,10 @@ import Plume.Syntax.Concrete.Expression (TypeConstructor (..))
 import Plume.TypeChecker.Checker.Monad
 import Plume.TypeChecker.Monad.Conversion
 import Plume.TypeChecker.TLIR qualified as Post
+
+{-# NOINLINE datatypes #-}
+datatypes :: IORef (Map Text (Map Text Int))
+datatypes = unsafePerformIO $ newIORef mempty
 
 synthDataType :: Infer
 synthDataType (Pre.EType (Annotation name generics) cons) = do
@@ -16,23 +22,31 @@ synthDataType (Pre.EType (Annotation name generics) cons) = do
         if null generics
           then TypeId name
           else TypeApp (TypeId name) $ map TypeVar convertedGenerics
-  cons' <- mapM (synthCons (convertedGenerics, header)) cons
+
+  (cons', m) <- mapAndUnzipM (synthCons (convertedGenerics, header)) cons
+
+  modifyIORef datatypes (M.insert name (M.unions m))
+
   return (TUnit, [Post.EType name cons'])
 synthDataType _ = throw $ CompilerError "Only data types are supported"
 
 synthCons
   :: ([TyVar], PlumeType)
   -> TypeConstructor Pre.PlumeType
-  -> Checker (TypeConstructor PlumeType)
+  -> Checker (TypeConstructor PlumeType, Map Text Int)
 synthCons (gens, header) (TVariable name) = do
   let scheme = Forall gens header
   insertEnv @"datatypeEnv" name scheme
 
-  pure $ TVariable name
+  let dataType = M.singleton name 0
+
+  pure (TVariable name, dataType)
 synthCons (gens, header) (TConstructor name args) = do
   args' <- mapM convert args
   let ty = args' :->: header
   let scheme = Forall gens ty
   insertEnv @"datatypeEnv" name scheme
 
-  pure $ TConstructor name args'
+  let dataType = M.singleton name (length args)
+
+  pure (TConstructor name args', dataType)
