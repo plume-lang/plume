3630a99300ab2403c74b84d8e411294b227f1e13
Sisypheus <contact@sisypheus.dev>
2024-04-23 11:59:53 +0200

    fix(bench): fixed benchmark runner script compatibility

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -1,4 +1,5 @@
-cd benchmark
+#!/bin/sh
+cd benchmark || exit
 rustc index.rs -C opt-level=3 -o index-rs
 javac Main.java
 # gcc -O3 benchmark/index.c -o index-c
@@ -7,5 +8,5 @@ hyperfine \
   "node index.js" \
   "./index-rs" \
   "java Main" \
-  "../bin/plume-vm ../example/closure.bin" \
+  "../bin/plume ../example/closure.bin" \
   --warmup 3 --export-markdown ../docs/BENCHMARK_RESULTS.md -N
