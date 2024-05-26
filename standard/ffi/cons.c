7ca60210a16b33f16f41a745a08c0b3b1feb9480
Sisypheus <contact@sisypheus.dev>
2024-05-26 19:38:39 +0200

    build(std): switched standard to CMake builder

--- standard/ffi/cons.c
+++ standard/ffi/cons.c
@@ -4,56 +4,56 @@
 #include <value.h>
 
 Value make_some(Value v) {
-  Value* values = malloc(sizeof(Value) * 4);
+  Value* values = gc_malloc(&gc, sizeof(Value) * 4);
   values[0] = MAKE_SPECIAL();
-  values[1] = MAKE_STRING("Option", 6);
-  values[2] = MAKE_STRING("Some", 4);
+  values[1] = MAKE_STRING("Option");
+  values[2] = MAKE_STRING("Some");
   values[3] = v;
 
   return MAKE_LIST(values, 4);
 }
 
 Value make_unit() {
-  Value* values = malloc(sizeof(Value) * 3);
+  Value* values = gc_malloc(&gc, sizeof(Value) * 3);
   values[0] = MAKE_SPECIAL();
-  values[1] = MAKE_STRING("unit", 4);
-  values[2] = MAKE_STRING("unit", 4);
+  values[1] = MAKE_STRING("unit");
+  values[2] = MAKE_STRING("unit");
 
   return MAKE_LIST(values, 3);
 }
 
 Value make_none() {
-  Value* values = malloc(sizeof(Value) * 3);
+  Value* values = gc_malloc(&gc, sizeof(Value) * 3);
   values[0] = MAKE_SPECIAL();
-  values[1] = MAKE_STRING("Option", 6);
-  values[2] = MAKE_STRING("None", 4);
+  values[1] = MAKE_STRING("Option");
+  values[2] = MAKE_STRING("None");
 
   return MAKE_LIST(values, 3);
 }
 
 Value MAKE_CHAR(char c) {
-  char str[2];
+  char* str = gc_malloc(&gc, sizeof(char) * 2);
   str[0] = c;
   str[1] = '\0';
 
-  return MAKE_STRING(str, 1);
+  return MAKE_STRING(str);
 }
 
 Value make_ok(Value v) {
-  Value* values = malloc(sizeof(Value) * 4);
+  Value* values = gc_malloc(&gc, sizeof(Value) * 4);
   values[0] = MAKE_SPECIAL();
-  values[1] = MAKE_STRING("Result", 6);
-  values[2] = MAKE_STRING("Ok", 2);
+  values[1] = MAKE_STRING("Result");
+  values[2] = MAKE_STRING("Ok");
   values[3] = v;
 
   return MAKE_LIST(values, 4);
 }
 
 Value make_err(Value v) {
-  Value* values = malloc(sizeof(Value) * 4);
+  Value* values = gc_malloc(&gc, sizeof(Value) * 4);
   values[0] = MAKE_SPECIAL();
-  values[1] = MAKE_STRING("Result", 6);
-  values[2] = MAKE_STRING("Error", 5);
+  values[1] = MAKE_STRING("Result");
+  values[2] = MAKE_STRING("Error");
   values[3] = v;
 
   return MAKE_LIST(values, 4);
@@ -88,7 +88,7 @@ void print_helper(Value v) {
     case TYPE_SPECIAL:
       printf("<special>");
       break;
-    
+
     case TYPE_UNKNOWN: {
       printf("<unknown>");
       break;
