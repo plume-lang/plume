63430b334f83a6a242b95ee73af46083f8bd09e5
Sisypheus <contact@sisypheus.dev>
2024-06-14 21:46:25 +0200

    fix(std): fixed http module

--- standard/c-ffi/http.c
+++ standard/c-ffi/http.c
@@ -5,47 +5,57 @@
 #include "cons.h"
 #include <stdio.h>
 
-size_t data_size = 0;
+struct MemoryStruct {
+  char *memory;
+  size_t size;
+  GarbageCollector gc;
+};
 
-size_t write_callback(void* contents, size_t size, size_t nmemb, char** response) {
+size_t write_callback(void* contents, size_t size, size_t nmemb, struct MemoryStruct* mem) {
   size_t realsize = size * nmemb;
-  *response = realloc(*response, realsize + 1);
-  if (*response == NULL) {
+  char* ptr = realloc(mem->memory, mem->size + realsize + 1);
+  if (ptr == NULL) {
     fprintf(stderr, "Failed to allocate memory\n");
     return 0;
   }
-  memcpy(*response, contents, realsize);
-  (*response)[realsize] = '\0';
-  data_size = realsize;
+
+  mem->memory = ptr;
+  memcpy(&(mem->memory[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+  
   return realsize;
 }
 
-Value ffi_fetch(size_t argc, Module* mod, Value* args) {
+Value fetch(size_t argc, Module* mod, Value* args) {
   ASSERT_FMT(argc == 1, "expected 1 argument, but got %zu", argc);
   Value url = args[0];
   ASSERT_FMT(get_type(url) == TYPE_STRING, "expected string, but got %s", type_of(url));
 
   const char* url_str = GET_STRING(url);
 
-  CURL* curl = curl_easy_init();
-  if (!curl) return make_err(mod->gc, MAKE_STRING(mod->gc, "Failed to initialize curl"));
-  
-  data_size = 0;
+  struct MemoryStruct mem;
+  mem.gc = gc;
+  mem.memory = malloc(1);
+  mem.size = 0;
 
-  // Store the response in a string
-  char* response = malloc(1);
+  CURL* curl = curl_easy_init();
+  if (!curl)
+    return make_err(mod->gc, MAKE_STRING(mod->gc, "Failed to initialize curl"));
   
   curl_easy_setopt(curl, CURLOPT_URL, url_str);
   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &mem);
   CURLcode res = curl_easy_perform(curl);
 
   if (res != CURLE_OK) {
-    curl_easy_cleanup(curl);
     char* err = (char*) curl_easy_strerror(res);
+    curl_easy_cleanup(curl);
     return make_err(mod->gc, MAKE_STRING(mod->gc, err));
   }
 
+  char* result = gc_strdup(&mod->gc, mem.memory);
+ 
   curl_easy_cleanup(curl);
-  return make_ok(mod->gc, MAKE_STRING(mod->gc, response));
+  return make_ok(mod->gc, MAKE_STRING(mod->gc, result));
 }
