c94f67db6375d06d5691e0cece38beea0c338202
Sisypheus <contact@sisypheus.dev>
2024-06-14 15:37:46 +0200

    feat(main): added basic language switcher for main CLI interface

--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,13 @@ import Plume.Compiler.TypeErasure.EraseType
 import Plume.Syntax.Parser.Modules.ParseImports
 import Plume.Syntax.Require.Resolution
 import Plume.Syntax.Translation.ConcreteToAbstract
+import Plume.Compiler.Bytecode.Syntax (Instruction)
 import Plume.Compiler.Javascript.Translate
+import Plume.Compiler.LLIR.Assembler
+import Plume.Compiler.Bytecode.Assembler
+import Plume.Compiler.Bytecode.Serialize
+-- import Plume.Compiler.Bytecode.Syntax (Instruction)
+import Plume.Compiler.Bytecode.Label hiding (labelPool)
 import Plume.TypeChecker.Checker
 -- import Plume.Syntax.Memory
 import Plume.Syntax.Blocks
@@ -45,7 +51,9 @@ setEncoding = id
 
 main :: IO ()
 main = setEncoding $ do
-  file_input <- maybeAt 0 <$> getArgs
+  args <- getArgs
+  let ext_type = fromString . fromMaybe "native" $ maybeAt 0 args
+  let file_input = maybeAt 1 args
   env <- lookupEnv "PLUME_PATH"
   mod' <- lookupEnv "PPM_PATH"
 
@@ -68,7 +76,7 @@ main = setEncoding $ do
             Nothing -> paths
 
       ppBuilding "Parsing file and dependencies..."
-      writeIORef extensionType "js"
+      writeIORef extensionType ext_type
 
       void $ checkModule (env, mod') file
 
@@ -81,9 +89,35 @@ main = setEncoding $ do
           runClosureConversion erased `with` \closed -> do
             desugared <- desugar closed
 
-            let js   = runTranslateJS desugared
-                code = createMainJSApp js
+            case ext_type of
+              "js" -> do
+                let js   = runTranslateJS desugared
+                    code = createMainJSApp js
 
-            writeFileText (replaceExtension file ".js") code
-            ppSuccess ("Bytecode written to " <> fromString (replaceExtension file ".js"))
+                writeFileText (replaceExtension file ".js") code
+                ppSuccess ("Bytecode written to " <> fromString (replaceExtension file ".js"))
+
+              _ -> do
+                (bytecode, natives', constants) <- runLLIRAssembler desugared
+                let nativeFuns = getNativeFunctions natives'
+                
+                globals' <- readIORef globals
+
+                (bytecode', labelPool) <- runUnlabelize bytecode
+
+                finalBytecode <- runBytecodeAssembler (labelPool, nativeFuns) bytecode'
+
+                sbc <- serialize (finalBytecode, natives', constants)
+                let new_path = file -<.> "bin"
+                writeFileLBS new_path sbc
+                ppSuccess ("Bytecode written to " <> fromString new_path)
     Nothing -> ppFailure "No input file provided"
+
+printBytecode :: [Instruction] -> IO ()
+printBytecode bytecode =
+  mapM_
+    ( \(i, instr) -> do
+        putStr (show i <> ": ")
+        print instr
+    )
+    (zip [0 :: Int ..] bytecode)
