1fce3bf1dd4bd499c93688d46161b12c689c0b1a
Sisypheus <contact@sisypheus.dev>
2024-07-02 13:42:21 +0200

    feat(lib): added Plume errors

---
+++ src/Control/Monad/Result.hs
@@ -0,0 +1,37 @@
+module Control.Monad.Result where
+
+import Language.Plume.Frontend.Parser qualified as P
+import Language.Plume.Syntax.Internal.Type qualified as Ty
+import GHC.Show qualified as S
+
+fromEither :: a -> Either b a -> a
+fromEither _ (Right a) = a
+fromEither a _ = a
+
+handle :: (MonadIO m, Show a) => Either a b -> (b -> m c) -> m c
+handle (Right a) f = f a
+handle (Left err) _ = liftIO $ print err >> exitFailure
+
+data PlumeError
+  = ParseError P.ParseError
+  | CyclicModuleDependency FilePath
+  | ModuleNotFound FilePath
+  | VariableNotFound Text
+  | TypeNotFound Text
+  | TypeMismatch Ty.PlumeType Ty.PlumeType
+
+instance Show PlumeError where
+  show (ParseError e) = P.errorBundlePretty e
+  show (CyclicModuleDependency path)
+    = "Cyclic module dependency detected in " <> show path
+  show (ModuleNotFound path)
+    = "Module " <> show path <> " not found"
+  show (VariableNotFound name)
+    = "Variable " <> show name <> " not found"
+  show (TypeNotFound name)
+    = "Type " <> show name <> " not found"
+  show (TypeMismatch t1 t2)
+    = S.show t1 <> " is not equivalent to " <> S.show t2
+
+showError :: P.ParseError -> String
+showError = P.errorBundlePretty
