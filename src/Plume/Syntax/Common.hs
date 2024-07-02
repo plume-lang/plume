e8550676e894578b567f0a40844777d869ce9c4f
Sisypheus <contact@sisypheus.dev>
2024-07-02 13:28:17 +0200

    feat: removed all old Plume files

--- src/Plume/Syntax/Common.hs
+++
@@ -1,6 +0,0 @@
-module Plume.Syntax.Common (module Common) where
-
-import Plume.Syntax.Common.Annotation as Common
-import Plume.Syntax.Common.Literal as Common
-import Plume.Syntax.Common.Pattern as Common
-import Plume.Syntax.Common.Type as Common
