8fc40379b22496fecd5a4ec1cbecf6cdc433c7cf
Thomas Vergne <contact@thomas-vergne.fr>
2024-08-04 18:32:37 +0200

    feat(std): updated native standard library

--- standard/native.js
+++ standard/native.js
@@ -147,4 +147,23 @@ module.exports = {
   },
 
   all: (promises) => Promise.all(promises),
+  add_big: (x, y) => x + y,
+  sub_big: (x, y) => x - y,
+  mul_big: (x, y) => x * y,
+  div_big: (x, y) => x / y,
+  mod_big: (x, y) => x % y,
+  int_to_big: (x) => new BigInt(x),
+  big_to_str: (x) => x.toString(),
+  eq_big: (x, y) => x === y,
+  lt_big: (x, y) => x < y,
+  create_big: (x) => new BigInt(x),
+
+  str_to_int: (x) => {
+    const res = parseInt(x);
+    if (isNaN(res)) {
+      return [null, 'Option', 'None'];
+    }
+
+    return [null, 'Option', 'Some', res];
+  }
 }
